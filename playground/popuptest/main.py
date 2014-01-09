d99a80af3da542fa1c774b3f4fc409c0b39d82f8
Ben Rousch <brousch@gmail.com>
2014-01-09 15:35:53 -0500

    Some more popup tests

--- playground/popuptest/main.py
+++ playground/popuptest/main.py
@@ -1,35 +1,65 @@
 import gc
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
  
  
 kv = """
+#:kivy 1.4
+
 <PopupTest>:
+    id: bl
+    orientation: "vertical"
+    popup: popup.__self__
+    canvas:
+        Color:
+            rgba: .18, .18, .18, .91
+        Rectangle:
+            size: self.size
+            pos: self.pos
+    Bubble:
+        size_hint: (None, None)
+        size: (150, 50)
+        pos_hint: {'center_x': .5, 'y': .6}
+        arrow_pos: 'bottom_mid'
+        orientation: 'horizontal'
+        BubbleButton:
+            text: 'This is'
+        BubbleButton:
+            text: 'a'
+        BubbleButton:
+            text: 'Bubble'
     Button:
-        text: "Press me for a popup"
-        on_press: root.make_popup()
-    Button:
-        text: "Press me to collect garbage"
-        on_press: root.collect_garbage()
+        text: 'press to show popup'
+        on_release: root.popup.open()
+    Popup:
+        id: popup
+        on_parent: if self.parent == bl: bl.remove_widget(self)
+        title: "An example popup"
+        content: popupcontent
+        Button:
+            id: popupcontent
+            text: "press to dismiss"
+            on_release: root.popup.dismiss()
 """
  
 Builder.load_string(kv)
  
  
 class PopupTest(BoxLayout):
+    mypopup = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super(PopupTest, self).__init__(**kwargs)
+        Clock.schedule_interval(self.collect_garbage, 10)
 
-    def make_popup(self):
-        popup = Popup(title='Test popup',
-                      content=Label(text='Now resize the window!'))
-        popup.open()
-
-    def collect_garbage(self):
+    def collect_garbage(self, dt=0):
+        print("Collecting garbage")
         gc.collect()
 
 class PopupTestApp(App):
