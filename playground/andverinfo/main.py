5a54abd1b3306f716d0816c088efa829305d5f2b
Ben Rousch <brousch@gmail.com>
2014-03-06 15:43:21 -0500

    fixing upper case format

--- playground/andverinfo/main.py
+++ playground/andverinfo/main.py
@@ -19,11 +19,11 @@ Builder.load_string(kv)
 class Test(BoxLayout):
     def __init__(self, **kwargs):
         super(Test, self).__init__(**kwargs)
-        self.add_widget(Label(text="CODENAME: {}".FORMAT(BuildVersion.CODENAME)))
-        self.add_widget(Label(text="INCREMENTAL: {}".FORMAT(BuildVersion.INCREMENTAL)))
-        self.add_widget(Label(text="RELEASE: {}".FORMAT(BuildVersion.RELEASE)))
-        self.add_widget(Label(text="SDK: {}".FORMAT(BuildVersion.SDK)))
-        self.add_widget(Label(text="SDK_INT: {}".FORMAT(BuildVersion.SDK_INT)))
+        self.add_widget(Label(text="CODENAME: {}".format(BuildVersion.CODENAME)))
+        self.add_widget(Label(text="INCREMENTAL: {}".format(BuildVersion.INCREMENTAL)))
+        self.add_widget(Label(text="RELEASE: {}".format(BuildVersion.RELEASE)))
+        self.add_widget(Label(text="SDK: {}".format(BuildVersion.SDK)))
+        self.add_widget(Label(text="SDK_INT: {}".format(BuildVersion.SDK_INT)))
  
  
 class TestApp(App):
